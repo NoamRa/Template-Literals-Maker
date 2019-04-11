c7c43d909b415785e13423ef222f22cadceae78d
NoamRa <noamraby@gmail.com>
2019-04-12 01:08:52 +0300

    temp

--- js/templateLiteralMaker.js
+++ js/templateLiteralMaker.js
@@ -1,54 +1,119 @@
 const inputId = "theInput";
 const outputId = "theOutput";
 const errorId = "theError";
+const copyButton = "copyButton";
 
 const enclosures = ["'", "`", `"`];
 
-document.getElementById(inputId)
-  .addEventListener('input', transformTextToTemplateLiteral);
+document
+  .getElementById(inputId)
+  .addEventListener("input", transformTextToTemplateLiteral);
+
+document
+  .getElementById(inputId)
+  .value = `"youve got " + 1 + ' example right ' + \`in this section\``
+
+document.getElementById(copyButton).addEventListener("click", copyToClipboard);
+
+function sanitize(str) {
+  const origLength = str.length;
+  let sanitized = str.trim();
+  if (sanitized.charAt(0) === "+") {
+    sanitized = sanitized.slice(1);
+  }
+  if (sanitized.charAt(sanitized.length - 1) === "+") {
+    sanitized = sanitized.slice(0, sanitized.length - 1);
+  }
+  
+  if (sanitized.length === origLength) {
+    return sanitized;
+  } else {
+    return sanitize(sanitized);
+  }
+}
 
 function parseInput(inputStr) {
-  
+  let plusIndexes = [];
   const inputLength = inputStr.length;
   let stack = [];
-  
-  for(let i = 0; i < inputLength; i++) {
+
+  for (let i = 0; i < inputLength; i++) {
     let currentChar = inputStr[i];
-    let lastCharInStack = stack[stack.length-1];
-    
-    // in not ", ', or `;
-    if (!enclosures.includes(currentChar)) {
+    let lastCharInStack = stack[stack.length - 1];
+
+    if (currentChar === "+" && stack.length === 0) {
+      plusIndexes.push(i);
+    } else if (!enclosures.includes(currentChar)) {
+      // in not ", ', or `;
       continue;
-    } 
-    else if (currentChar === lastCharInStack) {
+    } else if (currentChar === lastCharInStack) {
       stack.pop();
-    }
-    else {
+    } else {
       stack.push(currentChar);
     }
   }
 
   if (Boolean(stack.length)) {
-    document.getElementById(errorId).innerHTML = `something's not closed correctly`;
+    document.getElementById(errorId)
+    .innerHTML = `something's not closed correctly`;
   } else {
     document.getElementById(errorId).innerHTML = ``;
   }
-  const parsed = inputStr;
-
-  return parsed;
+  return plusIndexes;
 }
 
-function toTemplateLiterals(parsed) {
-  
-  const tl = `\`${parsed}\``
-  return tl;
+function toTemplateLiterals(inputStr, plusIndexes) {
+  let parts = [];
+  console.log("plusIndexes:", plusIndexes);
+  let currentStr = inputStr;
+  while (plusIndexes.length) {
+    let idx = plusIndexes.pop();
+    parts.unshift(currentStr.substring(idx + 1).trim());
+    currentStr = currentStr.substring(0, idx);
+  };
+  parts.unshift(currentStr.trim());
+
+  console.log(parts);
+  const processedParts = parts.map(part => {
+    if (enclosures.includes(part.charAt(0))) {
+      return part.substring(1, part.length - 1);
+    } else if (part.length === 0) {
+      return "";
+    } else if (part) {
+      try {
+        const something = JSON.parse(part); // TODO doesn't catch external strings
+        if (typeof something === "string") {
+          return `\${${part}}`;
+        } else {
+          return part;
+        }
+      } catch {
+        return part;
+      }
+    } else {
+      return `\${${part}}`;
+    }
+  });
+
+  const templateLiteral = `\`${processedParts.join("")}\``;
+  if (templateLiteral.includes("  ")) {
+    console.log("probably collapsing spaces")
+  }
+  return templateLiteral;
 }
 
 function transformTextToTemplateLiteral(evt) {
   console.log(evt.target.value);
-  const inputStr = evt.target.value;
-  const parsed = parseInput(inputStr);
-  const transformed = toTemplateLiterals(parsed);
-  document.getElementById(outputId).innerHTML = transformed;
+  const inputStr = sanitize(evt.target.value);
+  const plusIndexes = parseInput(inputStr);
+  const transformed = toTemplateLiterals(inputStr, plusIndexes);
+  document.getElementById(outputId).value = transformed;
 }
 
+
+function copyToClipboard(){
+  const templateLiteral = document.getElementById(outputId);
+  console.log(templateLiteral.value);
+  templateLiteral.select();
+  document.execCommand("copy");
+}
